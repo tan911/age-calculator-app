57aaa4baddf30d3f23cc8b3e2cdc9c51c12549df
Jovan Lanutan <jolanutan@gmail.com>
2023-04-16 02:27:58 +0800

    working with outputs

--- src/components/AgeCalc.jsx
+++ src/components/AgeCalc.jsx
@@ -1,23 +1,35 @@
 import { useSelector } from 'react-redux';
 
+import { Output } from '../layout/Content';
+
 function AgeCalc() {
   const age = useSelector((state) => {
     return state.calc.calcAge;
   });
 
-  // console.log(age.map((a) => a.day));
+  const year = age.map((a) => a.year);
+  const month = age.map((a) => a.month);
+  const day = age.map((a) => a.day);
+
+  const logYear = year.length !== 0 ? year : '--';
+  const logMonth = month.length !== 0 ? month : '--';
+  const logDay = day.length !== 0 ? day : '--';
+
   return (
-    <div>
-      <div>
-        <span>{age.map((a) => a.year)}</span>:years
-      </div>
-      <div>
-        <span>{age.map((a) => a.month)}</span>:months
-      </div>
-      <div>
-        <span>{age.map((a) => a.day)}</span>:days
-      </div>
-    </div>
+    <Output>
+      <p>
+        <span>{logYear} </span> 
+        years
+      </p>
+      <p>
+        <span>{logMonth} </span>
+        months
+      </p>
+      <p>
+        <span>{logDay} </span>
+        days
+      </p>
+    </Output>
   );
 }
 
