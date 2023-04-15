5c2ec48d79ca5ff6ea7aff0d0535c733fa9252da
Jovan Lanutan <jolanutan@gmail.com>
2023-04-16 02:31:07 +0800

    outputs styles

---
+++ src/layout/Content/output.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Output = styled.div`
+  p {
+    font-size: 7rem;
+    font-family: 'Poppins-ExtraBoldItalic';
+    color: ${({ theme }) => theme.colors.offBlack};
+    padding: 0;
+    line-height: ${({theme}) => theme.lineHeights[0]};
+  }
+
+  span {
+    color: ${({theme}) => theme.colors.purple};
+  }
+`;
+
+export { Output };
