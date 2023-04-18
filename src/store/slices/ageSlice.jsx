ef75f2a8aeddc25d492c074e6cd7a563fe31a93c
Jovan Lanutan <jolanutan@gmail.com>
2023-04-18 19:15:49 +0800

    removing checkers for calculations of age

--- src/store/slices/ageSlice.jsx
+++ src/store/slices/ageSlice.jsx
@@ -7,39 +7,11 @@ const ageSlice = createSlice({
   },
   reducers: {
     calculate(state, action) {
-      const now = new Date();
-
-      let DAY_VALUE, MONTH_VALUE, YEAR_VALUE;
-
-      const YEAR_INPUT = action.payload.year;
-      const MONTH_INPUT = action.payload.month;
-      const DAY_INPUT = action.payload.day;
-
-      let YEAR = now.getFullYear();
-      let MONTH = now.getMonth() + 1;
-      let DAY = now.getDate();
-
-      if (DAY < DAY_INPUT) {
-        DAY_VALUE = DAY - DAY_INPUT + 30;
-        MONTH -= 1;
-      } else {
-        DAY_VALUE = DAY - DAY_INPUT;
-      }
-
-      if (MONTH < MONTH_INPUT) {
-        MONTH_VALUE = MONTH - MONTH_INPUT + 12;
-        YEAR -= 1;
-      } else {
-        MONTH_VALUE = MONTH - MONTH_INPUT;
-      }
-
-      YEAR_VALUE = YEAR - YEAR_INPUT;
-
       const output = [
         {
-          year: Math.abs(YEAR_VALUE),
-          month: Math.abs(MONTH_VALUE),
-          day: Math.abs(DAY_VALUE)
+          year: Math.abs(action.payload.year),
+          month: Math.abs(action.payload.month),
+          day: Math.abs(action.payload.day)
         }
       ];
       state.calcAge = output;
